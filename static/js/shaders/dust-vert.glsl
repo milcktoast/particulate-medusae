f6ded55ac7fb3fceefc28250c55bc4e21eb05ef9
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-11-16 07:47:59 -0800

    Add secondary dust movement

--- static/js/shaders/dust-vert.glsl
+++ static/js/shaders/dust-vert.glsl
@@ -11,10 +11,11 @@ varying float centerDist;
 void main() {
   {{{chunks.color_vertex}}}
 
+  float offsetY = mod(position.y - 1.0 * time, area) - area * 0.5;
   vec3 offsetPosition = vec3(
-    position.x,
-    mod(position.y - 1.0 * time, area) - area * 0.5,
-    position.z);
+    position.x + sin(cos(offsetY * 0.1) * sin(offsetY * 0.1 + position.x * 0.1) * 2.0),
+    offsetY,
+    position.z + sin(cos(offsetY * 0.1) + sin(offsetY * 0.1 + position.z * 0.1) * 2.0));
 
   centerDist = length(offsetPosition);
 
