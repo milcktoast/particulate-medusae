a98ac05bf668885c57570c5cd44b30ea6d726e26
Jay Weeks <jay.patrick.weeks@gmail.com>
2014-12-29 05:26:46 -0500

    Update tail color

--- static/js/shaders/tail-frag.glsl
+++ static/js/shaders/tail-frag.glsl
@@ -24,8 +24,9 @@ void main() {
 
   vec2 uv = vUv;
   float color = (sin(uv.y * 1000.0) - sin((1.0 - uv.x) * 3.0 + 1.2)) * uv.y;
+  color = smoothstep(-0.65, 1.0, color);
 
-  gl_FragColor = vec4(vec3(color), color * opacity);
+  gl_FragColor = vec4(vec3(color) * diffuse, color * opacity);
 
   {{{chunks.envmap_fragment}}}
   {{{chunks.shadowmap_fragment}}}
