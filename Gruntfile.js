33f7992b956cd6377c217159599d98ebb4ee2926
Jay Weeks <jay.patrick.weeks@gmail.com>
2015-02-16 18:41:46 -0800

    Switch to jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,9 +10,9 @@ var CONFIG = {
 
 module.exports = function (grunt) {
   require('time-grunt')(grunt);
-  require('load-grunt-tasks')(grunt);
-
-  grunt.loadTasks('grunt/tasks');
+  require('jit-grunt')(grunt)({
+    loadTasks : 'grunt/tasks'
+  });
 
   [
     'autoprefixer',
