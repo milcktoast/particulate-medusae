6ef54b1d5cc370a91be98ff30d66261059546d3e
Jay Weeks <jay.patrick.weeks@gmail.com>
2015-11-08 16:48:36 -0800

    Fix alpha material shaders

--- static/glsl/shaders/alpha-vert.glsl
+++ static/glsl/shaders/alpha-vert.glsl
@@ -1,8 +1,6 @@
 {{{chunks.common}}}
 {{{chunks.uv_pars_vertex}}}
-{{{chunks.uv2_pars_vertex}}}
 {{{chunks.color_pars_vertex}}}
-{{{chunks.lerp_pos_pars_vertex}}}
 {{{chunks.logdepthbuf_pars_vertex}}}
 
 attribute float alpha;
@@ -10,12 +8,12 @@ varying float vAlpha;
 
 void main() {
   {{{chunks.uv_vertex}}}
-  {{{chunks.uv2_vertex}}}
   {{{chunks.color_vertex}}}
 
   {{{chunks.begin_vertex}}}
-  {{{chunks.lerp_pos_vertex}}}
+  {{{chunks.project_vertex}}}
   {{{chunks.logdepthbuf_vertex}}}
+
   {{{chunks.worldpos_vertex}}}
 
   vAlpha = alpha;
