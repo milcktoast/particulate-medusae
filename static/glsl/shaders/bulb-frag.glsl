690b4313fe8f928a59b2b9d801571a976e88f443
Jay Weeks <jay.patrick.weeks@gmail.com>
2015-11-21 20:32:02 -0800

    Update bulb shader

--- static/glsl/shaders/bulb-frag.glsl
+++ static/glsl/shaders/bulb-frag.glsl
@@ -4,9 +4,7 @@ uniform float opacity;
 varying vec2 vUv;
 varying vec3 vNormal;
 
-const float scale = 20.0;
-
-float accumulate(vec2 uv, float saturation) {
+float accumulate(vec2 uv, float saturation, float scale) {
   saturation -= sin(uv.x * 60.0) * 0.25 + sin(uv.x * 50.0 * scale) * 0.25 + 0.75;
 
   saturation -= sin(uv.y * sin(uv.x         * 5.0) * 5.0 * scale) * 0.05;
@@ -24,16 +22,16 @@ float accumulate(vec2 uv, float saturation) {
 }
 
 void main() {
+  vec2 uv = vUv;
   vec3 eye = vec3(0.0, 0.0, 1.0);
   vec3 normal = normalize(mat3(viewMatrix) * vNormal);
   float rim = 1.0 - max(dot(eye, normal), 0.0);
-
-  vec2 uv = vUv;
-  vec2 uvEye = uv + rim;
   float saturation = 0.0;
 
-  saturation += accumulate(uv, 2.2);
-  saturation += accumulate(uvEye, 0.8);
+  saturation += max(-0.25, accumulate(uv, 1.0, 20.0));
+  saturation += max(-0.25, accumulate(vec2(-rim * 0.5), 0.1, 1.0));
+  saturation += max(-0.25, accumulate(vec2(uv.x, rim), 1.25, 20.0));
+  saturation += max(-0.25, accumulate(vec2(rim, uv.y), 1.25, 30.0));
 
   gl_FragColor = vec4(
     mix(diffuseB, diffuse, saturation) * opacity,
